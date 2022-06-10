a5bb61698658032fd033981f3930c6e2366380a2
Jacob Doran <j.doran.admin@gmail.com>
2022-06-10 19:20:54 +0800

    (gitignore :add "emacs.d/elpa" :purge true)

--- emacs.d/elpa/wcheck-mode-2021/wcheck-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;; Generated package description from wcheck-mode.el  -*- no-byte-compile: t -*-
-(define-package "wcheck-mode" "2021" "General interface for text checkers" 'nil :authors '(("Teemu Likonen" . "tlikonen@iki.fi")) :maintainer '("Teemu Likonen" . "tlikonen@iki.fi") :keywords '("text" "spell" "check" "languages" "ispell") :url "https://github.com/tlikonen/wcheck-mode")
