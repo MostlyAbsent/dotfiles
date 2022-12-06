8fb251ab373f8d12cae0dbeb5f11dfd9cc7a6faa
Jacob Doran <jacobdoran@192-168-1-161.tpgi.com.au>
2022-12-06 19:44:02 +0800

    (config emacs :value add clj-refactor)

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -256,6 +256,15 @@
 (use-package org-roam-ui
   :ensure t)
 
+(use-package clj-refactor
+  :ensure t
+  :config
+  (defun my-clojure-mode-hook ()
+    (clj-refactor-mode 1)
+    (yas-minor-mode 1) ; for adding require/use/import statements
+    (cljr-add-keybindings-with-prefix "C-c C-m"))
+  (add-hook 'clojure-mode-hook #'my-clojure-mode-hook))
+
 (use-package ligature
   :ensure t
   :config
