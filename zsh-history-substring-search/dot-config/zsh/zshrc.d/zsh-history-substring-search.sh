da3cbc5749ef857fa064d422701b414d78371617
J <j.doran.admin@gmail.com>
2025-01-16 16:04:33 +0800

    Add substring search plugin configuration

---
+++ zsh-history-substring-search/dot-config/zsh/zshrc.d/zsh-history-substring-search.sh
@@ -0,0 +1,5 @@
+export ZSH_HISTORY_SUBSTRING=TRUE
+source $(brew --prefix)/share/zsh-history-substring-search/zsh-history-substring-search.zsh
+
+bindkey '^[[A' history-substring-search-up
+bindkey '^[[B' history-substring-search-down
