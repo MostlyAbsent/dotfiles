b2c90709c96d2cdfb1632f48b7944ad83ff47a47
J <j.doran.admin@gmail.com>
2025-04-19 12:35:00 +0800

    nvim: markdown: add render-markdown plugin

--- nvim/dot-config/nvim/lua/custom/plugins/mini.lua
+++ nvim/dot-config/nvim/lua/custom/plugins/mini.lua
@@ -30,6 +30,8 @@ return {
 
     require('mini.pairs').setup()
 
+    require('mini.icons').setup()
+
     -- Simple and easy statusline.
     --  You could remove this setup call if you don't like it,
     --  and try some other statusline plugin
--- nvim/dot-config/nvim/lua/custom/plugins/obsidian.nvim.lua
+++ nvim/dot-config/nvim/lua/custom/plugins/obsidian.nvim.lua
@@ -56,7 +56,7 @@ return {
       },
     },
     ui = {
-      enable = true,
+      enable = false,
       checkboxes = {
         [' '] = { char = '󰄱', hl_group = 'ObsidianTodo' },
         ['x'] = { char = '', hl_group = 'ObsidianDone' },
